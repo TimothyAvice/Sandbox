73f39f5c5bfd8a2b5184ac7030ea0bb298d554ef
jc439081 <timothy.avicedubuisson@my.jcu.edu.au>
2018-03-13 09:33:53 +1000

    Add name printer

--- oddName.py
+++ oddName.py
@@ -1,3 +1,17 @@
 """
 Timothy Avice Du Buisson
 """""
+import string
+count = 0
+
+while True:
+    user_name = input("Please enter your name: ")
+    for char in user_name:
+        if char.upper() in string.ascii_uppercase:
+            count += 1
+    if count != 0:
+        print(user_name[::2])
+        break
+    else:
+        print("Not valid input")
+        continue
