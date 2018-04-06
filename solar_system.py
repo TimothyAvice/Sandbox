73337da073ecbcaec693ee3df0b5a7b0b32c1aba
Timothy Avice Du Buisson <timothy.avicedubuisson@my.jcu.edu.au>
2018-04-06 22:02:51 +1000

    Alter generate_ellipse static equation

--- solar_system.py
+++ solar_system.py
@@ -14,18 +14,20 @@ class Planet(Ellipse):
     def __init__(self, au=1, radius=20, speed = 1):
         self.au = au
         self.radius = radius
-        self.orbit = generate_ellipse()
+        generate_ellipse()
+        # self.orbit = generate_ellipse()
         self.speed = speed
 
     @staticmethod
     def generate_ellipse():
-        x = random.randint(1,40)
         y = random.randint(1,40)
         a = random.randint(1,40)
         b = random.randint(1,40)
-        for x < 40
+        x = int(-a)
+        for x in range(-a,a):
+            x += 1
             y = math.sqrt((((a^2)(b^2)) - ((b^2)(x^2)))/(a^2))
-        x = math.sqrt((((a^2)(b^2)) - ((a^2)(y^2)))/(b^2))
+        # x = math.sqrt((((a^2)(b^2)) - ((a^2)(y^2)))/(b^2))
 
     def build(self):
 
