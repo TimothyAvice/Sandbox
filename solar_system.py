5ed1ac8a06207c9a69512fe692214b71ce16ebf2
Timothy Avice Du Buisson <timothy.avicedubuisson@my.jcu.edu.au>
2018-04-07 10:57:46 +1000

    Add kivy code

--- solar_system.py
+++ solar_system.py
@@ -9,27 +9,32 @@ from kivy.uix.widget import Widget
 from kivy.clock import Clock
 
 
-class Planet(Ellipse):
+class Planet(App):
     # AU is placeholder for distance from the sun
-    def __init__(self, au=1, radius=20, speed = 1):
+    def __init__(self, au=1, radius=20, speed=1):
+        super().__init__()
         self.au = au
         self.radius = radius
-        generate_ellipse()
         # self.orbit = generate_ellipse()
         self.speed = speed
 
     @staticmethod
     def generate_ellipse():
-        y = random.randint(1,40)
-        a = random.randint(1,40)
-        b = random.randint(1,40)
+        y = random.randint(1, 40)
+        a = random.randint(1, 40)
+        b = random.randint(1, 40)
         x = int(-a)
-        for x in range(-a,a):
-            x += 1
-            y = math.sqrt((((a^2)(b^2)) - ((b^2)(x^2)))/(a^2))
+        # for x in range(-a,a):
+        #     x += 1
+        #     y = math.sqrt((((a^2)(b^2)) - ((b^2)(x^2)))/(a^2))
         # x = math.sqrt((((a^2)(b^2)) - ((a^2)(y^2)))/(b^2))
-
-    def build(self):
-
+        return a, b
 
     def orbit(self):
+        self.root.ids.orbit.size_hint_x, self.root.ids.orbit.size_hint_y = self.generate_ellipse()
+
+    def build(self):
+        self.root = Builder.load_file("solar_system.kv")
+        return self.root
+
+Planet().run()
