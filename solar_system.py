e502fba6c24e3ddd5141c6f9d4c0090ec2bc0b3c
jc439081 <timothy.avicedubuisson@my.jcu.edu.au>
2018-04-10 11:09:46 +1000

    Theres no going back

--- solar_system.py
+++ solar_system.py
@@ -8,33 +8,47 @@ from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 
+class planet(Ellipse):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class Display(Widget):
+    def __init__(self):
+        super().__init__()
+        # y = random.randint(1, 40)
+        a = random.randint(1, 40)
+        b = random.randint(1, 40)
+        for i in range(-5, 5):
+            y = math.sqrt((((a**2)*(b**2)) - ((b**2)*(i**2)))/(a**2))
+            for n in range(-5, 5):
+                x = math.sqrt((((a**2)*(b**2)) - ((a**2)*(n**2))) / (b ^ 2))
+        # for x in range(0, 10):
+        #     y = 5
+                with self.canvas:
+                    Color(1, 1, 1, 1)
+                    planet(pos=(x+400, y+400), size=(2,2))
+
+
+        # x = math.sqrt((((a^2)*(b^2)) - ((a^2)*(y^2)))/(b^2))
+        # return a, b
+
+    # def orbit(self):
+        # self.root.ids.orbit.pos = self.generate_ellipse()
+
 
 class Planet(App):
     # AU is placeholder for distance from the sun
-    def __init__(self, au=1, radius=20, speed=1):
-        super().__init__()
-        self.au = au
-        self.radius = radius
-        # self.orbit = generate_ellipse()
-        self.speed = speed
+    # def __init__(self, au=1, radius=20, speed=1):
+    #     super().__init__()
+    #     self.au = au
+    #     self.radius = radius
+    #     # self.orbit = generate_ellipse()
+    #     self.speed = speed
+    #     Display().generate_ellipse()
 
     def build(self):
         self.root = Builder.load_file("solar_system.kv")
         return self.root
-
-    @staticmethod
-    def generate_ellipse():
-        y = random.randint(1, 40)
-        a = random.randint(1, 40)
-        b = random.randint(1, 40)
-        x = int(-a)
-        # for x in range(-a,a):
-        #     x += 1
-        #     y = math.sqrt((((a^2)(b^2)) - ((b^2)(x^2)))/(a^2))
-        # x = math.sqrt((((a^2)(b^2)) - ((a^2)(y^2)))/(b^2))
-        return a, b
-
-    def orbit(self):
-        self.root.ids.orbit.size_hint_x, self.root.ids.orbit.size_hint_y = self.generate_ellipse()
 
 Planet().run()
