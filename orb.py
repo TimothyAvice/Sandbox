0e321770284a0dbf6f685f17917db3e41e44dcbb
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2018-04-19 19:57:56 +1000

    Change to orb program

--- orb.py
+++ orb.py
@@ -9,7 +9,7 @@ from kivy.clock import Clock
 from kivy.core.window import Window
 
 
-class Planet(Ellipse):
+class Orb(Ellipse):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -18,15 +18,22 @@ class Display(Widget):
     def __init__(self):
         super().__init__()
         Window.size = (1000, 800)
+        self.increment = 0.005
         self.path_a = random.uniform(50, 300)
         self.path_b = random.uniform(50, 300)
         self.x = -self.path_a
         self.y = 400
         self.z = -400
         self.count = 1
-        self.planet = self.generate_planet()
-        Clock.schedule_interval(self.follow_orbit, 0.001)
-        self.canvas.add(self.planet)
+        # self.planet = self.generate_planet()
+        Clock.schedule_interval(self.follow_path, self.increment)
+        # self.canvas.add(self.planet)
+
+    def change_increment_down(self):
+        self.increment -= 1
+
+    def change_increment_up(self):
+        self.increment += 1
 
     def variables(self):
         self.path_a = random.uniform(50, 300)
@@ -35,23 +42,25 @@ class Display(Widget):
         self.y = 400
         self.z = -400
         self.count = 1
-        self.planet = self.generate_planet()
-        self.canvas.add(self.planet)
+        # self.planet = self.generate_planet()
+        # self.canvas.add(self.planet)
 
     @staticmethod
-    def planet_color():
+    def orb_color():
         red = random.uniform(0.0, 1.0)
         blue = random.uniform(0.0, 1.0)
         green = random.uniform(0.0, 1.0)
-        return Color(red, blue, green)
+        gamma = random.uniform(0.5, 1.0)
+        return Color(red, blue, green, gamma)
 
-    def follow_orbit(self, _):
-        self.planet.pos = (self.x + 500, self.y + 400)
+    def follow_path(self, _):
+        # self.planet.pos = (self.x + 500, self.y + 400)
         if self.count == 1:
             if self.x <= self.path_a:
                 self.y = (((((self.path_a ** 2) * (self.path_b ** 2)) - ((self.path_b ** 2) *
                                                                          (self.x ** 2))) / (self.path_a ** 2)) ** 0.5)
-                self.x += 1
+                self.x += 10
+                self.generate_orb()
             else:
                 self.count += 1
                 self.x = self.path_a
@@ -60,23 +69,29 @@ class Display(Widget):
                 self.y = (((((self.path_a ** 2) * (self.path_b ** 2)) - ((self.path_b ** 2) *
                                                                          (self.x ** 2))) / (self.path_a ** 2)) ** 0.5)
                 self.y = -self.y
-                self.x -= 1
+                self.x -= 10
+                self.generate_orb()
             else:
                 self.count -= 1
                 self.x = -self.path_a
 
-    def generate_planet(self):
+    def generate_orb(self):
         size = random.uniform(10, 50)
-        return Planet(pos=(self.x, self.y), size=(size, size))
+        with self.canvas:
+            self.orb_color()
+            Orb(pos=(self.x+500, self.y+400), size=(size, size))
+
+    # def generate_planet(self):
+    #     return Planet(pos=(self.x, self.y), size=(self.planet_size, self.planet_size))
 
 
-class PlanetApp(App):
+class OrbApp(App):
     def __init__(self):
         super().__init__()
 
     def build(self):
-        self.root = Builder.load_file("planet.kv")
+        self.root = Builder.load_file("orb.kv")
         return self.root
 
 
-PlanetApp().run()
+OrbApp().run()
