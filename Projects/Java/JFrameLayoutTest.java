4494ce440e157aa235b921c4e19fabb73e8c1af8
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2019-10-12 14:06:57 +1000

    Sandbox Structure commit

---
+++ Projects/Java/JFrameLayoutTest.java
@@ -0,0 +1,12 @@
+package Java;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JFrameLayoutTest extends JFrame {
+
+    public JFrameLayoutTest() throws HeadlessException {
+        super("Layout test");
+        
+    }
+}
